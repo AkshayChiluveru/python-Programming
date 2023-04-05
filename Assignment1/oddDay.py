d9d75f50ebf5e6d2f740851d1c2ee6422177b1e5
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-04-05 21:34:02 +0530

    Update oddDay.py

--- Assignment1/oddDay.py
+++ Assignment1/oddDay.py
@@ -1,7 +1,7 @@
-numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+numbers = int(input("Enter Number : "))
 even = 0
 odd = 0
-for num in numbers:
+for num in range (1,numbers+1):
     if num % 2 == 0:
         even += 1
     else:
