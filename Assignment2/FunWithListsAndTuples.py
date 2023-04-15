9dfa8672e37454a9a03994f1b7e48a998d4eb359
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-04-15 12:03:17 +0530

    updated assignment2

--- Assignment2/FunWithListsAndTuples.py
+++ Assignment2/FunWithListsAndTuples.py
@@ -1,5 +1,14 @@
 def sort_tuple_list(tuple_list):
-    return sorted(tuple_list, key=lambda x: x[-1])
+    n = len(tuple_list)
+    i = 0
+    while i < n-1:
+        j = i+1
+        while j < n:
+            if tuple_list[i][-1] > tuple_list[j][-1]:
+                tuple_list[i], tuple_list[j] = tuple_list[j], tuple_list[i]
+            j += 1
+        i += 1
+    return tuple_list
 
 tuple_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 sorted_tuple_list = sort_tuple_list(tuple_list)
