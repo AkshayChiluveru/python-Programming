c31866943c2c8b0fed83e0a714d692cfdd363944
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-04-17 12:39:37 +0530

    Input from the user

--- Assignment3/calculateTheUpperAndTheLowerCase.py
+++ Assignment3/calculateTheUpperAndTheLowerCase.py
@@ -6,9 +6,13 @@ def count_upper_lower(string):
             upper_count += 1
         elif char.islower():
             lower_count += 1
-    return (upper_count, lower_count)
+    return upper_count, lower_count
 
-my_string = "The quick Brow Fox"
+my_string = input("Enter a string: ")
+
+
 upper_count, lower_count = count_upper_lower(my_string)
-print("No. of Upper case characters :", upper_count)  # Output: 3
-print("No. of Lower case Characters :", lower_count)
+
+
+print('No. of Upper case characters :', upper_count)
+print('No. of Lower case characters :', lower_count)
