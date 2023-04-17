146224e12c432163e22e42e1fc1e07d65f2ec344
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-04-17 12:43:17 +0530

    input from the user

--- Assignment3/stringInsideTheFunction.py
+++ Assignment3/stringInsideTheFunction.py
@@ -1,4 +1,8 @@
 def reverse_string(string):
     return string[::-1]
-my_string = "1234abcd"
-print(reverse_string(my_string))
+
+my_string = input("Enter a string: ")
+
+reversed_string = reverse_string(my_string)
+
+print(reversed_string)
