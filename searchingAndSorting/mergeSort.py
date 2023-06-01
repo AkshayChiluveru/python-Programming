74ec0f0b4fb65550e39adb8c11294371de66d82b
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-06-01 20:41:28 +0530

    Merge Sort

---
+++ searchingAndSorting/mergeSort.py
@@ -0,0 +1,39 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+
+    return merged
+
+# Test the merge sort function
+arr = [5, 3, 8, 2, 1, 9, 4]
+sorted_arr = merge_sort(arr)
+print("Sorted array:", sorted_arr)
