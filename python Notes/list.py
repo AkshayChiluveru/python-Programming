c1169d78dc24641567ed64167e7884bd98ca2964
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-06-23 01:29:59 +0530

    modules

---
+++ python Notes/list.py
@@ -0,0 +1,43 @@
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+
+thislist[1:3] = ["blackcurrant", "watermelon"]
+
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+
+thislist[1:2] = ["blackcurrant", "watermelon"]
+
+print(thislist)
+
+
+
+thislist = ["apple", "banana", "cherry"]
+
+thislist.insert(2, "watermelon")
+
+print(thislist) 
+
+
+# Access Items
+# List items are indexed and you can access them by referring to the index number:
+
+# Example:
+# Print the second item of the list:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+
+# Example
+# Return the third, fourth, and fifth item:
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
