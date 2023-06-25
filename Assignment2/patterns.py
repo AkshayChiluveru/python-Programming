03ea111c63aabbbf5e48cae6c87460e8349b8362
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-06-25 16:36:53 +0530

    Patterns

---
+++ Assignment2/patterns.py
@@ -0,0 +1,94 @@
+# # Square pattern
+
+# n = int(input("Enter the number:"))
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end=" ")
+#     print()
+
+
+# # right angled triangle
+
+# n = int(input("Enter the number:"))
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+
+# # Decreasing right angled triangle
+
+# n = int(input("Enter the number:"))
+# for i in range(n):
+#     for j in range(i,n):
+#         print("*",end=" ")
+#     print()
+
+
+
+# n = int(input("Enter the number:"))
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for k in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+
+
+# square
+
+# n = 5
+# for i in range(n):
+#     for j in range(n):
+#         print("#" , end=" ")
+#     print()
+
+
+# right triangle
+# n = 5
+# for i in range(n):
+#     for j in range(i+1):
+#         print("#" , end=" ")
+#     print()
+
+
+# right downward triangle
+# n = 5
+# for i in range(n):
+#     for j in range(n-i):
+#         print("#" , end=" ")
+#     print()
+
+
+
+# left triangle 
+
+# n = 5
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" " , end=" ")
+#     for j in range(i+1):
+#         print("*" , end=" ")
+#     print()
+
+
+# left downward triangle
+# n = 5
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" " , end=" ")
+#     for j in range(n-i):
+#         print("*" , end=" ")
+#     print()
+
+
+#  Triangle
+
+n = 5
+for i in range(n):
+    for j in range(n-i):
+        print(" " , end="")
+    for j in range(i+1):
+        print("*" , end=" ")
+    print()
