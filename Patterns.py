c0801fee844ed2236f7b399e073a1d2760b1c184
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-06-25 18:36:21 +0530

    patterns

--- Patterns.py
+++ Patterns.py
@@ -1,96 +1,121 @@
-# # Square pattern
-
-# n = int(input("Enter the number:"))
+# square pattern
+# n = 5
 # for i in range(n):
 #     for j in range(n):
-#         print("*",end=" ")
+#         print("*" , end=" ")
 #     print()
 
 
-# # right angled triangle
-
-# n = int(input("Enter the number:"))
+# right angled triangle:
+# n = 5
 # for i in range(n):
 #     for j in range(i+1):
-#         print("*",end=" ")
+#         print("*" , end=" ")
 #     print()
 
 
-# # Decreasing right angled triangle
+# printing odd number of patterns
 
-# n = int(input("Enter the number:"))
-# for i in range(n):
-#     for j in range(i,n):
-#         print("*",end=" ")
+# n = 5
+# k = 1
+# for i in range(1,n+1):
+#     for j in range(1,k+1):
+#         print("*" , end=" ")
+#     k = k + 2
 #     print()
 
 
+# triangle
+# n =5
+# for i in range(0,n):
+#     for j in range(0,n-i-1):
+#         print(end=" ")
+#     for j in range(0,i+1):
+#         print("*",end=" ")
+#     print()
 
-# n = int(input("Enter the number:"))
-# for i in range(n):
-#     for j in range(i,n):
+# odd number of triangle paattern
+
+# n =5
+# for i in range(0,n):
+#     for j in range(0,n-i-1):
+#         print(end=" ")
+#     for j in range(0,2*i+1):
+#         print("*",end="")
+#     print()
+
+# reverse triangle pattern
+
+# n = 5
+# for i in range(0,n):
+#     for j in range(0,i):
+#         print(end=" ")
+#     for j in range(0,n-i):
+#         print("*",end=" ") 
+#     print()
+
+# diamond 
+
+# n =5
+# for i in range(0,n-1):
+#     for j in range(0,n-i-1):
+#         print(end=" ")
+#     for j in range(0,i+1):
+#         print("*",end=" ")
+#     print()
+# for i in range(0,n):
+#     for j in range(0,i):
+#         print(end=" ")
+#     for j in range(0,n-i):
+#         print("*",end=" ") 
+#     print()
+
+# reversed right angle triangle 
+# n = 5
+# for i in range(0,n):
+#     for j in range(0,n-i):
+#         print("*",end=" ")
+#     print()
+
+# reverse left angle triangle
+# n = 5
+# for i in range(0,n):
+#     for j in range(0,i):
 #         print(" ",end=" ")
-#     for k in range(i+1):
+#     for j in range(0,n-i):
 #         print("*",end=" ")
 #     print()
 
 
 
-#  practicing patters
-
-# square
-
+# left angled triangle
 # n = 5
-# for i in range(n):
-#     for j in range(n):
-#         print("#" , end=" ")
+# for i in range(0,n):
+#     for j in range(0,n-i):
+#         print(" ",end=" ")
+#     for j in range(0,i+1):
+#         print("*",end=" ")
 #     print()
 
 
-# right triangle
-# n = 5
-# for i in range(n):
+
+
+# numbers pattern
+# n = int(input("Enter the number: "))
+# num = 1
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(num, end=" ")
+#         num = num+1
+#     print()
+
+
+# string pattern program 
+
+# string = input("Enter the string: ")
+# l = len(string)
+# for i in range(l):
 #     for j in range(i+1):
-#         print("#" , end=" ")
+#         print(string[j], end=" ")
 #     print()
 
-
-# right downward triangle
-# n = 5
-# for i in range(n):
-#     for j in range(n-i):
-#         print("#" , end=" ")
-#     print()
-
-
-
-# left triangle 
-
-# n = 5
-# for i in range(n):
-#     for j in range(n-i):
-#         print(" " , end=" ")
-#     for j in range(i+1):
-#         print("*" , end=" ")
-#     print()
-
-
-# left downward triangle
-# n = 5
-# for i in range(n):
-#     for j in range(i+1):
-#         print(" " , end=" ")
-#     for j in range(n-i):
-#         print("*" , end=" ")
-#     print()
-
-
-#  Triangle
-
-n = 5
-for i in range(n):
-    for j in range(n-i):
-        print(" " , end="")
-    for j in range(i+1):
-        print("*" , end=" ")
-    print()
