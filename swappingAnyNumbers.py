428b686f349163e7feb0598fbecd86ec1ecb1006
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-06-27 10:47:45 +0530

    swapping elements

--- swappingAnyNumbers.py
+++
@@ -1,9 +0,0 @@
-my_array = []
-
-user_input = input("Enter elements: ")
-
-elements = user_input.split()
-
-my_array.extend(elements)
-
-print("Array:", my_array)
--- swappingfirstAndLastElements.py
+++ swappingfirstAndLastElements.py
@@ -10,3 +10,29 @@ myList[length-1] = temp
 myList[0],myList[-1] = myList[-1],myList[0]
 
 print(myList)
+
+
+# user input
+
+myList = []
+
+user_input = input("Enter elements: ")
+
+elements = user_input.split()
+
+myList.extend(elements)
+
+
+
+print("Array:", myList)
+
+# Approach 1
+length = len(myList)
+temp = myList[0]
+myList[0] = myList[length-1]
+myList[length-1] = temp
+
+# Approach 2
+myList[0],myList[-1] = myList[-1],myList[0]
+
+print("Array:", myList)
