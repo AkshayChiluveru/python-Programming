978f0375cff3b86cc7e6e63167f6f98b57982304
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-07-01 23:32:24 +0530

    tuple

--- even.py
+++ even.py
@@ -1,6 +1,8 @@
-a = int(input("Enter the start value: "))
-b = int(input("Enter the end value: "))
-c = set(range(a,b,2))
-print(c)
-print("The length of c is: ",len(c))
+# a = int(input("Enter the start value: "))
+# b = int(input("Enter the end value: "))
+# c = set(range(a,b,2))
+# print(c)
+# print("The length of c is: ",len(c))
 
+num = tuple("Akshay")
+print(num)
