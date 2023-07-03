578a386443647752182e2a5de909754595da2abf
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-07-03 13:07:33 +0530

    show seats

--- DemoProject/Project.py
+++ DemoProject/Project.py
@@ -18,3 +18,28 @@ class TicketInfo:
             self.user_info()
         else:
             print("Enter a valid option")
+
+    
+    def show_seats(self):
+        for i in range(self.row + 1):
+            for j in range(self.col + 1):
+                # row =0
+                if i == 0:
+                    # col =0
+                    if j == 0:
+                        print(" ",end = " ")
+                        # row 0 and col 1 -> n 
+                    else:
+                        print(j,end = " ")
+                else:
+                    # col 0 and row 1 -> n
+                    if j == 0:
+                        print(i,end = " ")
+                        # row and col !=0
+                    else:
+                        if self.seat_status(i, j):
+                            print("B",end = " ")
+                        else:
+                            print("S",end = " ")
+            print()
+    
