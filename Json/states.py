cddef521d3d4deaac338d22f35a55781b698b93b
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-07-04 12:40:26 +0530

    deleting and creating a new file

--- Json/states.py
+++ Json/states.py
@@ -1,7 +1,22 @@
 import json
 
+# with open('json/states.json') as f:
+#     data = json.load(f)
+
+# for state in data['states']:
+#     print(state['name'], "--",state['area_codes'])
+
+
+
+
+# deleting and creating a new file:
+
 with open('json/states.json') as f:
     data = json.load(f)
 
 for state in data['states']:
-    print(state['name'], "--",state['area_codes'])
+    del state['area_codes']
+
+with open('json/new_states.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
