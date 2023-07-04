d5d29f1536b28701c8f2b5f3b1f833c56f4d53c5
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-07-04 18:47:37 +0530

    adding trainer details

--- certification/admin_panel.py
+++ certification/admin_panel.py
@@ -4,6 +4,7 @@ import random
 class Admin_panel:
     def __init__(self):
         self.moduledetails = {}
+        self.trainer_details = {}
 
     def admin_login(self,username,password):
         if username == "admin" and password == "admin":
@@ -23,9 +24,39 @@ class Admin_panel:
             }
         self.moduledetails[key] = moduledata
         with open("certification/add_module.json", "w") as f:
-            json.dump(self.moduledetails, f)
+            json.dump(self.moduledetails, f,indent = 2)
         return self.moduledetails
+
+# d = Admin_panel()
+# d.add_module("python", "8 weeks")
+# d.add_module("mySQL", "4 weeks")
+
+    def addTrainer(self):
+        trainer_id = random.randint(1,100)
+        name = input("enter the name: ")
+        gender = input("Enter the gender: ")
+        mobile = int(input("Enter the mobile: "))
+        experience = input("enter the Experience: ")
+        qualification = input("Enter the Qualification: ")
+        email_id = input("Enter the email id: ")
+        password = input("Enter the pass word")
+        trainer_data = {
+            "trainer_id": trainer_id,
+            "name" : name,
+            "gender": gender,
+            "mobile": mobile,
+            "experience": experience,
+            "qualification": qualification,
+            "email_id": email_id,
+            "password": password
+        }
+        self.trainer_details[trainer_id] = trainer_data
+        with open("certification/add_trainer.json", "w") as f:
+            json.dump(self.trainer_details, f,indent = 2)
+        return self.trainer_details
+    
 
 d = Admin_panel()
 d.add_module("python", "8 weeks")
 d.add_module("mySQL", "4 weeks")
+d.addTrainer()
