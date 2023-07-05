0485b1ecc481640f052da0d722400557cc1382b7
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-07-05 12:38:02 +0530

    adding student

--- certification/admin_panel.py
+++ certification/admin_panel.py
@@ -2,10 +2,12 @@ import json
 import random
 
 class Admin_panel:
+    student_list = []
     def __init__(self):
         self.moduledetails = {}
         self.trainer_details = {}
         self.batch_details = {}
+        self.student_details = {}
 
     def admin_login(self,username,password):
         if username == "admin" and password == "admin":
@@ -15,7 +17,7 @@ class Admin_panel:
     def add_module(self,modulename,duration):
         topic_list = []
         key = random.randint(1,100)
-        topic_size = int(input("Enter number of topics: "))
+        topic_size = int(input("Enter number of topics that you want to add: "))
         for i in range(topic_size):
             topic = input(f"Enter topic name {i}: ")
             topic_list.append(topic)
@@ -67,19 +69,61 @@ class Admin_panel:
     def add_batch(self,module,trainer,student):
         key = random.randint(1,100)
         batch_data = {
-            "ID": key,
             "module" : module,
             "trainer" : trainer,
             "student" : student
         }
-        self.batch_details = batch_data
+        self.batch_details[key] = batch_data
         with open("certification/add_batch.json", "w") as f:
             json.dump(self.batch_details, f,indent = 2)
         return self.batch_details
     
 
+# d = Admin_panel()
+# d.add_module("python", "8 weeks")
+# d.add_module("mySQL", "4 weeks")
+# d.addTrainer()
+# d.add_batch("DSA","Akshay","Student")
+
+
+    def add_student(self):
+        key = random.randint(1,100)
+        student_size = int(input("Enter the number of students you want to add: "))
+        for i in range(1,student_size+1):
+            print(f"Enter the details of the student {i}: ")
+            name = input("Enter the name of the student: ")
+            gender = input("Enter the gender of the student: ")
+            mobile = input("Enter the mobile number: ")
+            qualification = input("Enter the Qualification: ")
+            email_id = input("Enter the email id: ")
+            password = input("Enter the pass word")
+            student_data = {
+                "name" : name,
+                "gender": gender,
+                "mobile": mobile,
+                "qualification": qualification,
+                "email_id": email_id,
+                "password": password
+            }
+
+            Admin_panel.student_list.append(student_data) 
+        self.student_details[key] = Admin_panel.student_list
+        with open("certification/add_student.json", "w") as f:
+            json.dump(self.student_details, f,indent = 2)
+            
+        return self.student_details
+            
+
+
 d = Admin_panel()
-d.add_module("python", "8 weeks")
-d.add_module("mySQL", "4 weeks")
-d.addTrainer()
-d.add_batch("DSA","Akshay","Student")
+print("First batch is: ",d.add_batch("python","deepak","akshay"))
+print("second batch is: ",d.add_batch("MySql","naveena", "naina"))
+print("-"*40)
+print("First batch is: ",d.add_module("python","8 weeks"))
+print("second batch is: ",d.add_module("mySql","4 weeks"))
+print("-"*40)
+print(d.addTrainer())
+print(d.addTrainer())
+print("-"*40)
+print(d.add_student())
+print(d.add_student())
