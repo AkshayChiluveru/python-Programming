2b3f36ae70991e0e222235625969fad4e2a84dc1
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-07-13 10:40:42 +0530

    simple calculator

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# Python Program to Make a Simple Calculator
+# Simple Calculator by Using Functions
+
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    return x / y
+
+print("choose the option ")
+print("1. Add")
+print("2. Subtract")
+print("3. multiply")
+print("4. Divide")
+    
+
+while True:
+    choice = float(input("Choose the option from the above"))
+    if choice == '1'
