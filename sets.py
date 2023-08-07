51baa5b36a569d3b0f1d65599f2fc3db47a27615
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2023-08-07 19:31:18 +0530

    Difference between List and Array

---
+++ sets.py
@@ -0,0 +1,13 @@
+# create your own directory like this d = {1:3,2:6,3:11,5:27}
+d={}
+for i in range(1,6):
+    d[i] = i*i +2
+
+print(d)
+
+
+
+# can we create for this  1:1,2:4,3:37,4:256 ,5:3125
+
+
+# {A : 65,B:66,C:67, D:68, E:69}
